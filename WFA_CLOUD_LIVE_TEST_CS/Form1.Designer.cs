0c36cdc0f87f881aa3ee76da9ff029b6364ef776
koonp <koonp@FTL-KPHAROCKER>
2022-02-10 09:24:34 +0700

    Add project files.

---
+++ WFA_CLOUD_LIVE_TEST_CS/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace WFA_CLOUD_LIVE_TEST_CS
+{
+    partial class frmSoftware
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grb1 = new System.Windows.Forms.GroupBox();
+            this.chk3 = new System.Windows.Forms.CheckBox();
+            this.chk1 = new System.Windows.Forms.CheckBox();
+            this.chk2 = new System.Windows.Forms.CheckBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chk6 = new System.Windows.Forms.CheckBox();
+            this.chk5 = new System.Windows.Forms.CheckBox();
+            this.chk4 = new System.Windows.Forms.CheckBox();
+            this.grb1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grb1
+            // 
+            this.grb1.Controls.Add(this.chk3);
+            this.grb1.Controls.Add(this.chk1);
+            this.grb1.Controls.Add(this.chk2);
+            this.grb1.Location = new System.Drawing.Point(78, 62);
+            this.grb1.Name = "grb1";
+            this.grb1.Size = new System.Drawing.Size(200, 138);
+            this.grb1.TabIndex = 0;
+            this.grb1.TabStop = false;
+            this.grb1.Text = "อาหารจานด่วน";
+            // 
+            // chk3
+            // 
+            this.chk3.AutoSize = true;
+            this.chk3.Location = new System.Drawing.Point(6, 72);
+            this.chk3.Name = "chk3";
+            this.chk3.Size = new System.Drawing.Size(63, 19);
+            this.chk3.TabIndex = 0;
+            this.chk3.Text = "15 THB";
+            this.chk3.UseVisualStyleBackColor = true;
+            // 
+            // chk1
+            // 
+            this.chk1.AutoSize = true;
+            this.chk1.Location = new System.Drawing.Point(6, 22);
+            this.chk1.Name = "chk1";
+            this.chk1.Size = new System.Drawing.Size(63, 19);
+            this.chk1.TabIndex = 0;
+            this.chk1.Text = "35 THB";
+            this.chk1.UseVisualStyleBackColor = true;
+            // 
+            // chk2
+            // 
+            this.chk2.AutoSize = true;
+            this.chk2.Location = new System.Drawing.Point(6, 47);
+            this.chk2.Name = "chk2";
+            this.chk2.Size = new System.Drawing.Size(63, 19);
+            this.chk2.TabIndex = 0;
+            this.chk2.Text = "25 THB";
+            this.chk2.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(78, 237);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.BtnOK_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(452, 237);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chk6);
+            this.groupBox1.Controls.Add(this.chk5);
+            this.groupBox1.Controls.Add(this.chk4);
+            this.groupBox1.Location = new System.Drawing.Point(327, 62);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 138);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "อาหารจานด่วน";
+            // 
+            // chk6
+            // 
+            this.chk6.AutoSize = true;
+            this.chk6.Location = new System.Drawing.Point(6, 72);
+            this.chk6.Name = "chk6";
+            this.chk6.Size = new System.Drawing.Size(63, 19);
+            this.chk6.TabIndex = 0;
+            this.chk6.Text = "65 THB";
+            this.chk6.UseVisualStyleBackColor = true;
+            // 
+            // chk5
+            // 
+            this.chk5.AutoSize = true;
+            this.chk5.Location = new System.Drawing.Point(6, 47);
+            this.chk5.Name = "chk5";
+            this.chk5.Size = new System.Drawing.Size(63, 19);
+            this.chk5.TabIndex = 0;
+            this.chk5.Text = "55 THB";
+            this.chk5.UseVisualStyleBackColor = true;
+            // 
+            // chk4
+            // 
+            this.chk4.AutoSize = true;
+            this.chk4.Location = new System.Drawing.Point(6, 22);
+            this.chk4.Name = "chk4";
+            this.chk4.Size = new System.Drawing.Size(63, 19);
+            this.chk4.TabIndex = 0;
+            this.chk4.Text = "45 THB";
+            this.chk4.UseVisualStyleBackColor = true;
+            // 
+            // frmSoftware
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 338);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.grb1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "frmSoftware";
+            this.Text = "ร้านอาหาร";
+            this.grb1.ResumeLayout(false);
+            this.grb1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grb1;
+        private System.Windows.Forms.CheckBox chk3;
+        private System.Windows.Forms.CheckBox chk2;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chk6;
+        private System.Windows.Forms.CheckBox chk5;
+        private System.Windows.Forms.CheckBox chk4;
+        private System.Windows.Forms.CheckBox chk1;
+    }
+}
+
---
+++ WFA_CLOUD_LIVE_TEST_CS/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_CLOUD_LIVE_TEST_CS
+{
+    public partial class frmSoftware : Form
+    {
+        public frmSoftware()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOK_Click(object sender, EventArgs e)
+        {
+            double v = validate();
+            if (v == 0)
+            {
+                MessageBox.Show("เลือกรายการไอเหี้ย!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show(v.ToString(), "ยอดชำระ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private double validate()
+        {
+            int cost=0;
+            if (chk1.Checked) { cost += 35; }
+            if (chk2.Checked) { cost += 25; }
+            if (chk3.Checked) { cost += 15; }
+            if (chk4.Checked) { cost += 45; }
+            if (chk5.Checked) { cost += 55; }
+            if (chk6.Checked) { cost += 65; }
+            return cost;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
